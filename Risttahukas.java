b95082e8e203ee9f3b63fd408f409f2dd6e99b56
Reit Blum <reit.blum@tigu.hk.tlu.ee>
2016-10-14 13:11:24 +0300

    Risttahukas

---
+++ Risttahukas.java
@@ -0,0 +1,16 @@
+public class Risttahukas extends KorgusegaKujund {
+
+	double pikkus, laius;
+	public Risttahukas(double pikkus, double laius, double korgus){
+		super(korgus);
+		this.pikkus=pikkus;
+		this.laius=laius;
+		
+	}
+	
+	public double pohjaPindala(){
+		
+		return pikkus*laius;
+	}
+	
+}
